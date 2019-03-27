e507baecc3958753cf96d732270d3e750a544cb6
Raul <raulsg93@gmail.com>
2019-03-27 13:58:56 -0500

    imported components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header'
+import ProjectCard from './components/ProjectCard/ProjectCard'
 
 class App extends Component {
   render() {

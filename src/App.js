94c8e98c2479f1e4015c8387044b1e4e409caaa1
Raul <raulsg93@gmail.com>
2019-03-27 17:23:12 -0500

    image reformatted- see if build accepts paths

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import ImgMediaCard from './components/ImgMediaCard/ImgMediaCard'
+import barpar from './images/barpar.png'
+import fortress from './images/fortress.jpg'
+import train from './images/trainschedule.png'
+import liri from './images/Liri.png'
 
 
 
@@ -12,23 +16,23 @@ class App extends Component {
     let projects = [
       {
         title: 'BarPar',
-        sum: 'MERN Stack- Inventory, Employee and Bar Relations Application. Worked on MongoDB Schemas and Inventory components',
-        image: '/static/media/barpar.ab9b526a.png'
+        sum: '1 of 7 contributors in this MERN Stack project. Inventory, Employee and Bar Relations Application. Worked on MongoDB Schemas and Inventory components',
+        image: barpar
       },
       {
         title: 'Fortress Fabrication WebPage',
         sum: 'React front-end app. Soon to be an active online webstore. Built this for a fabrication shop',
-        image: '/static/media/fortress.5622592d.jpg'
+        image: fortress
       },
       {
         title: 'Train Scheduler',
         sum: 'Moment.js and firebase application, built on a static HTML page. Application gives user next available train relative to current time given User input in minutes.',
-        image: '/static/media/trainschedule.74a6ac43.png'
+        image: train
       },
       {
         title: 'LIRI- Node Project',
         sum: 'Pure node.js project using dotenv library and accessing Spotify and Twitter APIs',
-        image: '/static/media/Liri.89b97854.png'
+        image: liri
       }
 
                           ];
